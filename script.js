a263381ce23bdab48ee383011314b981320e3de7
John Darrin <john.m.darrin@gmail.com>
2021-12-16 13:14:37 -0500

    timer function, nf issue

--- script.js
+++ script.js
@@ -6,24 +6,25 @@ let stopElem = document.querySelector('.stop');
 let resetElem = document.querySelector('.reset');
 let timerElem = document.querySelector('.timer');
 
-let timer = 0;
+let counter = 0;
+let timeLeft = 65;
 
 pomodoroElem.addEventListener('click', () => {
     console.log("pomodoro");
-    timer = 25;
-    updateTimer(timer);
+    counter = 25;
+    updateTimer(counter);
 });
 
 shortBreakElem.addEventListener('click', () => {
     console.log("shortbreak");
-    timer = 5;
-    updateTimer(timer);
+    counter = 5;
+    updateTimer(counter);
 });
 
 longBreakElem.addEventListener('click', () => {
     console.log("longbreak");
-    timer = 10;
-    updateTimer(timer);
+    counter = 10;
+    updateTimer(counter);
 });
 
 startElem.addEventListener('click', () => {
@@ -36,9 +37,24 @@ stopElem.addEventListener('click', () => {
 
 resetElem.addEventListener('click', () => {
     console.log("reset");
+    timerElem.innerHTML = timer;
 });
 
-
-function updateTimer(timer){
-    timerElem.innerHTML = timer;
+function convertSeconds(s) {
+    let min = Math.floor(s / 60);
+    let sec = s % 60;
+    return nf(min,2) + ":" + nf(sec,2);
 }
+
+
+function updateTimer(counter){
+    timerElem.innerHTML = convertSeconds((timeLeft - counter));
+    return counter;
+};
+
+function timerStart(){
+    counter++;
+    timerElem.innerHTML = convertSeconds((timeLeft - counter));
+};
+
+setInterval(timerStart, 1000);
