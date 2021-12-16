e14580b0bbccc48c5afd3a6369c20fac94b407f4
John Darrin <john.m.darrin@gmail.com>
2021-12-16 11:47:35 -0500

    timer functions

--- script.js
+++ script.js
@@ -4,4 +4,41 @@ let longBreakElem = document.querySelector('.longBreakButton');
 let startElem = document.querySelector('.start');
 let stopElem = document.querySelector('.stop');
 let resetElem = document.querySelector('.reset');
+let timerElem = document.querySelector('.timer');
 
+let timer = 0;
+
+pomodoroElem.addEventListener('click', () => {
+    console.log("pomodoro");
+    timer = 25;
+    updateTimer(timer);
+});
+
+shortBreakElem.addEventListener('click', () => {
+    console.log("shortbreak");
+    timer = 5;
+    updateTimer(timer);
+});
+
+longBreakElem.addEventListener('click', () => {
+    console.log("longbreak");
+    timer = 10;
+    updateTimer(timer);
+});
+
+startElem.addEventListener('click', () => {
+    console.log("start");
+});
+
+stopElem.addEventListener('click', () => {
+    console.log("stop");
+});
+
+resetElem.addEventListener('click', () => {
+    console.log("reset");
+});
+
+
+function updateTimer(timer){
+    timerElem.innerHTML = timer;
+}
