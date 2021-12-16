f795df5b02e7bbcdb852b46780e11d95fff2d268
John Darrin <john.m.darrin@gmail.com>
2021-12-16 11:30:54 -0500

    timer function

---
+++ script.js
@@ -0,0 +1,7 @@
+let pomodoroElem = document.querySelector('.pomodoroButton');
+let shortBreakElem = document.querySelector('.shortBreakButton');
+let longBreakElem = document.querySelector('.longBreakButton');
+let startElem = document.querySelector('.start');
+let stopElem = document.querySelector('.stop');
+let resetElem = document.querySelector('.reset');
+
